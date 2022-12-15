5dea170cad39846b909931bebd2534846545a1b9
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2022-12-14 18:05:30 -0600

    add ansifilter

--- pkg/handle/handle.go
+++ pkg/handle/handle.go
@@ -20,49 +20,56 @@ func Haltingstate(w http.ResponseWriter, r *http.Request) {
        fmt.Fprintf(w,"<br><p style='color:red'>error reading chat history export:\n<br> %v\n<br>", err)
        fmt.Fprintf(w,"try reloading\n<br>")
        fmt.Fprintf(w, "<img src='img/haltingstate.jpg'><br></p>")
-     }
-
-     data := TgHist{}
-
-     err = json.Unmarshal([]byte(file), &data)
-     if err != nil {
-       fmt.Fprintf(w,"<br><p style='color:red'>error unmarshalling json:\n<br> %v\n<br>", err)
-       fmt.Fprintf(w,"try reloading\n<br>")
-       fmt.Fprintf(w, "<img src='img/haltingstate.jpg'><br></p>")
-     }
-     rand.Seed(time.Now().UnixNano())
-     min := 0
-     max := len(data.Messages)
-
-     i := rand.Intn(max - min) + min
-     //	for i := 0; i < len(data.Messages); i++ {
-     var ok bool
-     for !ok {
-     if data.Messages[i].From == "Synth" || data.Messages[i].From == "Skycoin" {
-       if data.Messages[i].Text != "" {
-         ok = true
-       }
-     }
-     if !ok {
-       i = rand.Intn(max - min) + min
-     }
-     }
-
-     shcmd :=`/usr/bin/bash -c`
-     cmd := fmt.Sprintf(`%s "_var=\"%s\" ; _var=\"${_var#*\[map\[text\:}\" ; _var=\"${_var/type:link]/\n}\" ; _var=\"${_var%]}\" ; echo ${_var}"`, shcmd, fmt.Sprintf("%s", data.Messages[i].Text))
-     res, err := script.Exec(cmd).String()
-     fmt.Fprintf(w,"<a href='https://t.me/Skycoin/%d' title='https://t.me/Skycoin/%d'>https://t.me/Skycoin/%d</a>\n<br>", data.Messages[i].ID, data.Messages[i].ID, data.Messages[i].ID)
-     fmt.Fprintf(w,"%s\n<br>", data.Messages[i].Date)
-     if err != nil {
-       fmt.Fprintf(w,"<br><p style='color:red'>error during script.Exec:\n<br> %v\n<br>", err)
-       fmt.Fprintf(w,"try reloading\n<br></p>")
-       fmt.Fprintf(w, "<img src='img/haltingstate.jpg'><br>")
      } else {
-       fmt.Fprintf(w,"\n<br>%s\n<br>\n<br>", res)
-     }
-     fmt.Fprintf(w,"-%s\n<br>\n<br>", data.Messages[i].From)
-     fmt.Fprintf(w,"</body></html>")
-}
+		 data := TgHist{}
+		 err = json.Unmarshal([]byte(file), &data)
+		 if err != nil {
+			 fmt.Fprintf(w,"<br><p style='color:red'>error unmarshalling json:\n<br> %v\n<br>", err)
+			 fmt.Fprintf(w,"try reloading\n<br>")
+			 fmt.Fprintf(w, "<img src='img/haltingstate.jpg'><br></p>")
+		 }
+		 rand.Seed(time.Now().UnixNano())
+		 min := 0
+		 max := len(data.Messages)
+		 i := rand.Intn(max - min) + min
+		 //	for i := 0; i < len(data.Messages); i++ {
+		 var ok bool
+		 for !ok {
+			 if data.Messages[i].From == "Synth" || data.Messages[i].From == "Skycoin" {
+				 if data.Messages[i].Text != "" {
+					 ok = true
+				 }
+			 }
+			 if !ok {
+				 i = rand.Intn(max - min) + min
+			 }
+		 }
+		 shcmd :=`/usr/bin/bash -c`
+		 os.Remove("/tmp/haltingstate")
+		 os.Remove("/tmp/haltingstate0")
+		 err := os.WriteFile("/tmp/haltingstate", []byte(fmt.Sprintf("%s",data.Messages[i].Text)), 0644)
+		 if err != nil {
+			 fmt.Fprintf(w, "%v",err)
+		 }
+		 //
+		 cmd := fmt.Sprintf(`%s "ansifilter --html -i /tmp/haltingstate -o /tmp/haltingstate0 ; tail -n +5 /tmp/haltingstate0 | head -n -1 | tee /tmp/haltingstate0"`, shcmd)
+		 res, err := script.Exec(cmd).String()
+		 fmt.Fprintf(w,"<a href='https://t.me/Skycoin/%d' title='https://t.me/Skycoin/%d'>https://t.me/Skycoin/%d</a>\n<br>", data.Messages[i].ID, data.Messages[i].ID, data.Messages[i].ID )
+		 fmt.Fprintf(w,"%s\n<br>", data.Messages[i].Date)
+		 if err != nil {
+			 fmt.Fprintf(w,"<br><p style='color:red'>error during script.Exec:\n<br> %v\n<br>", err)
+			 fmt.Fprintf(w,"try reloading\n<br></p>")
+			 fmt.Fprintf(w,"command:\n<br>")
+			 fmt.Fprintf(w, cmd)
+			 fmt.Fprintf(w,"\n<br>")
+			 fmt.Fprintf(w, "<img src='img/haltingstate.jpg'><br>")
+			 } else {
+				 fmt.Fprintf(w,"\n<br>%s\n<br>\n<br>", res)
+			 }
+			 fmt.Fprintf(w,"-%s\n<br>\n<br>", data.Messages[i].From)
+			 fmt.Fprintf(w,"</body></html>")
+		 }
+	 }
 
 
 
